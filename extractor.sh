c597415aaafc18588855b7384850d1469b84275b
Jude Nwadinobi <121777365+d4rk5id3r@users.noreply.github.com>
2024-10-13 19:57:01 +0100

    Add files via upload

---
+++ extractor.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check if the plugin.txt file exists
+if [[ ! -f "plugin.txt" ]]; then
+    echo "File plugin.txt not found!"
+    exit 1
+fi
+
+# Create the extracted_plugins directory if it doesn't exist
+mkdir -p "extracted_plugins"
+
+# Navigate to the extracted_plugins directory
+cd "extracted_plugins" || exit
+
+# Extract 3000 random plugin names from plugin.txt
+PLUGIN_NAMES=$(shuf -n 3 "../plugin.txt")
+
+# Read each plugin name
+while IFS= read -r PLUGIN_NAME; do
+    # Set the SVN URL for the plugin
+    SVN_URL="https://plugins.svn.wordpress.org/$PLUGIN_NAME/trunk/"
+
+    # Create a directory for the plugin if it doesn't exist
+    mkdir -p "$PLUGIN_NAME"
+
+    # Navigate to the plugin directory
+    cd "$PLUGIN_NAME" || exit
+
+    # Checkout the latest version from the SVN repository
+    svn checkout "$SVN_URL" .
+
+    echo "Downloaded the latest version of $PLUGIN_NAME to $(pwd)"
+    
+    # Navigate back to the extracted_plugins directory
+    cd ..
+
+done <<< "$PLUGIN_NAMES"
---
+++ single_grep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the directory to scan
+DIRECTORY="extracted_plugins"
+
+# Check if the directory exists
+if [ ! -d "$DIRECTORY" ]; then
+    echo "Directory $DIRECTORY does not exist. Exiting..."
+    exit 1
+fi
+
+# Run ripgrep and filter the results with grep, then save to report.txt
+rg --no-heading "echo.*\\\$_GET" "$DIRECTORY" | \
+grep "\.php:" | \
+grep -v -e "(\$_GET" -e "( \$_GET" -e "esc_" -e "admin_url" -e "(int)" -e htmlentities > report.txt
+
+# Notify user that the report is generated
+echo "Results have been saved to report.txt"
