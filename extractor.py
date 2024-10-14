488bbe20d083742689b2b0b0fcaf16ffea401838
Jude Nwadinobi <121777365+d4rk5id3r@users.noreply.github.com>
2024-10-14 10:56:50 +0100

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -15,7 +15,7 @@ with open("plugin.txt", "r") as file:
     plugin_names = file.readlines()
 
 # Randomly select 3 plugin names
-selected_plugins = random.sample(plugin_names, 3)
+selected_plugins = random.sample(plugin_names, 19394)
 
 # Navigate to the extracted_plugins directory
 os.chdir("extracted_plugins")
