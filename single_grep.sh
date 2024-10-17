af284547134770173513a8a7ab89d0ac23c984b0
Jude Nwadinobi <121777365+d4rk5id3r@users.noreply.github.com>
2024-10-17 15:16:40 +0100

    Update single_grep.sh

--- single_grep.sh
+++ single_grep.sh
@@ -10,7 +10,7 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 # Run ripgrep and save results to report.txt
-rg --glob '*.php' --no-ignore-vcs -e '\$_GET' -e '\$_POST' -e '\$_REQUEST' -e '\$_SERVER\['\''REQUEST_URI'\''\]' -e '\$_SERVER\['\''PHP_SELF'\''\]' -e '\$_SERVER\['\''HTTP_REFERER'\''\]' -e '\$_COOKIE' -e 'add_query_arg\(' -e 'remove_query_arg\(' -e '\.\s*\$([a-z])\w+/g' "$DIRECTORY" > report.txt
+rg --glob '*.php' --no-ignore-vcs '\b(echo|print|printf|sprintf|die|wp_die)\s*\(' "$DIRECTORY" > report.txt
 
 # Notify user that the report is generated
 echo "Results have been saved to report.txt"
