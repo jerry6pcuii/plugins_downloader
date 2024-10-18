ed63d7abf4d97f9c46210aacc2d5f7b7dc7222cf
Jude Nwadinobi <121777365+d4rk5id3r@users.noreply.github.com>
2024-10-19 00:43:28 +0100

    Update single_grep.sh

--- single_grep.sh
+++ single_grep.sh
@@ -10,7 +10,7 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 # Run ripgrep and save results to report.txt
-rg --no-heading "echo.*\\\$_GET" | grep "\.php:" | grep -v -e "(\$_GET" -e "( \$_GET" -e "esc_" -e "admin_url" -e "(int)" -e htmlentities "$DIRECTORY" > report.txt
+rg --no-heading "echo.*\\\$_GET" | grep "\.php:" | grep -v -e "(\$_GET" -e "( \$_GET" -e "esc_" -e "admin_url" -e "(int)" -e htmlentities -e sanitize_text_field "$DIRECTORY" > report.txt
 
 # Notify user that the report is generated
 echo "Results have been saved to report.txt"
