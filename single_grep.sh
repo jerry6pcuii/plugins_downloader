dc4e9c92765718683157f2ea554ff6fc1a96f346
Jude Nwadinobi <121777365+d4rk5id3r@users.noreply.github.com>
2024-10-19 00:36:36 +0100

    Update single_grep.sh

--- single_grep.sh
+++ single_grep.sh
@@ -10,7 +10,7 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 # Run ripgrep and save results to report.txt
- rg --glob '*.php' --no-ignore-vcs -e '\$_GET' "$DIRECTORY" > report.txt
+ rg --glob '*.php' --no-ignore-vcs -e '\$_POST' "$DIRECTORY" > report.txt
 
 # Notify user that the report is generated
 echo "Results have been saved to report.txt"
