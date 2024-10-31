ca65cc0969c643fd9aac3d59568885049c834f97
Jude Nwadinobi <121777365+d4rk5id3r@users.noreply.github.com>
2024-10-31 09:45:24 +0100

    Update single_grep.sh

--- single_grep.sh
+++ single_grep.sh
@@ -10,7 +10,7 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 # Run ripgrep and save results to report.txt
-rg -r 'move_uploaded_file' "$DIRECTORY" > report.txt
+rg -i 'add_query_arg' --glob '*.php' "$DIRECTORY" > report.txt
 
 # Notify user that the report is generated
 echo "Results have been saved to report.txt"
