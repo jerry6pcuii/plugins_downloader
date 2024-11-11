37e2226c4e60bdb93ab918283ebaf77fc371c08d
Jude Nwadinobi <121777365+d4rk5id3r@users.noreply.github.com>
2024-11-11 10:47:36 +0100

    Update single_grep.sh

--- single_grep.sh
+++ single_grep.sh
@@ -10,7 +10,7 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 # Run ripgrep and save results to report.txt
-rg "move_uploaded_file|file_put_contents|fwrite|fputs|copy|fputcsv|rename|WP_Filesystem_Direct::put_contents|WP_Filesystem_Direct::move|WP_Filesystem_Direct::copy" --iglob '*.php' -C 3 "$DIRECTORY" > report.txt
+rg "move_uploaded_file\(" --iglob '*.php' -C 3 "$DIRECTORY" > report.txt
 
 # Notify user that the report is generated
 echo "Results have been saved to report.txt"
