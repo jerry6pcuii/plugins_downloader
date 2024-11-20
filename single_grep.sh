e06f14cc71b27d9dd7497e8e2a99cc7cca35ebd3
Jude Nwadinobi <121777365+d4rk5id3r@users.noreply.github.com>
2024-11-20 21:01:06 +0100

    Update single_grep.sh

--- single_grep.sh
+++ single_grep.sh
@@ -10,7 +10,8 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 # Run ripgrep and save results to report.txt
-rg --no-heading "add_query_arg\s*\(\s*array\s*\(\s*[^)]*\)\s*\)" -C 4 "$DIRECTORY" > report.txt
+rg --no-heading "echo.*\\\$_POST" | grep "\.php:" | grep -v -e "(\$_GET" -e "( \$_GET" -e "esc_" -e "admin_url" -e "(int)" -e "htmlentities" -e "sanitize_" -e "wp_nonce_field" -e "wp_kses" -e "sanitize_text_field" -e "sanitize_email" -e "sanitize_url" -e "esc_attr" -e "esc_html" -e "wp_kses_post" -e "esc_js"
+ -C 10 "$DIRECTORY" > report.txt
 
 # Notify user that the report is generated
 echo "Results have been saved to report.txt"
